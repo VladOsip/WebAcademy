a04dc0dc5727625628c7e4a7e053e4851f3abe2c
VladOsip <o.s.i.p@icloud.com>
2020-01-19 04:21:27 +0200

    homework12_done

--- src/homework12/homework12.js
+++ src/homework12/homework12.js
@@ -1,32 +1,72 @@
-import './homework12.scss';
+import "./homework12.scss";
+import "./traffic-lighter.scss";
+const trafficLighterRoot = document.querySelector(".traffic-lighter");
+const lights = trafficLighterRoot.querySelectorAll(".traffic-lighter__light");
+const togglerBtn = trafficLighterRoot.querySelector(
+  ".traffic-lighter__toggler"
+);
 
-const red = document.querySelector(“.red”);
-const yellow = document.querySelector(“.yellow”);
-const green = document.querySelector(“.green”);
-const btnStart = document.getElementById(“start”);
-const btnStop = document.getElementById(“stop”);
-const toggleOff = () => {
-  red.style.backgroundColor = “”;
-  yellow.style.backgroundColor = “”;
-  green.style.backgroundColor = “”;
-  btnStart.disabled = true; // блокируем кнопку Старт
-  btnStop.disabled = false;
+let isEnabled = false;
+let activeIndex = 0;
+let intervalId = 0;
+
+const toggleOffAll = () => {
+  for (let i = 0; i < lights.length; i++) {
+    lights[i].classList.remove("active");
+  }
 };
-const toggleOn = () => {
-  red.onclick = function() {
-    toggleOff();
-    red.style.backgroundColor = “red”;
-  };
-  yellow.onclick = function() {
-    toggleOff();
-    yellow.style.backgroundColor = “yellow”;
-  };
-  green.onclick = function() {
-    toggleOff();
-    green.style.backgroundColor = “green”;
-  };
-  btnStart.disabled = false;
-  btnStop.disabled = true;
+
+const activateLight = () => {
+  lights[activeIndex].classList.add("active");
 };
-btnStart.addEventListener(“click”, toggleOff);
-btnStop.addEventListener(“click”, toggleOn);
+
+for (let i = 0; i < lights.length; i++) {
+  lights[i].onclick = function() {
+    if (isEnabled) {
+      toggleOffAll();
+      activeIndex = i;
+      clearInterval(intervalId);
+      activateLight();
+      startAutoChange();
+    }
+  };
+}
+
+const changeIndex = () => {
+  if (activeIndex + 1 < lights.length) {
+    activeIndex += 1;
+  } else {
+    activeIndex = 0;
+  }
+};
+
+const toggleOffLighter = () => {
+  isEnabled = false;
+  toggleOffAll();
+  activeIndex = 0;
+  clearInterval(intervalId);
+};
+
+const startAutoChange = () => {
+  intervalId = setInterval(() => {
+    toggleOffAll();
+    changeIndex();
+    activateLight();
+  }, 1000);
+};
+
+const toggleOnLighter = () => {
+  isEnabled = true;
+  activateLight(0);
+  startAutoChange();
+};
+
+const togglerClickHandler = () => {
+  if (isEnabled) {
+    toggleOffLighter();
+  } else {
+    toggleOnLighter();
+  }
+};
+
+togglerBtn.onclick = togglerClickHandler;
