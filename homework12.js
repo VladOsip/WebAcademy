f5ee2eed91d9b3844b9dcac692785480e3e50138
VladOsip <o.s.i.p@icloud.com>
2020-01-19 04:21:44 +0200

    Updates

--- homework12.js
+++ homework12.js
@@ -90,10 +90,110 @@
 /*!**************************************!*\
   !*** ./src/homework12/homework12.js ***!
   \**************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: F:\\WebAcademy_new\\static-builder-master\\src\\homework12\\homework12.js: Unexpected character '“' (3:35)\n\n\u001b[0m \u001b[90m 1 | \u001b[39m\u001b[36mimport\u001b[39m \u001b[32m'./homework12.scss'\u001b[39m\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 2 | \u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 3 | \u001b[39m\u001b[36mconst\u001b[39m red \u001b[33m=\u001b[39m document\u001b[33m.\u001b[39mquerySelector(“\u001b[33m.\u001b[39mred”)\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m   | \u001b[39m                                   \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 4 | \u001b[39m\u001b[36mconst\u001b[39m yellow \u001b[33m=\u001b[39m document\u001b[33m.\u001b[39mquerySelector(“\u001b[33m.\u001b[39myellow”)\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 5 | \u001b[39m\u001b[36mconst\u001b[39m green \u001b[33m=\u001b[39m document\u001b[33m.\u001b[39mquerySelector(“\u001b[33m.\u001b[39mgreen”)\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 6 | \u001b[39m\u001b[36mconst\u001b[39m btnStart \u001b[33m=\u001b[39m document\u001b[33m.\u001b[39mgetElementById(“start”)\u001b[33m;\u001b[39m\u001b[0m\n    at Object.raise (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:6420:17)\n    at Object.getTokenFromCode (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:7118:10)\n    at Object.getTokenFromCode (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:3650:18)\n    at Object.nextToken (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:6630:12)\n    at Object.next (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:6559:10)\n    at Object.parseSubscript (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:8658:12)\n    at Object.parseSubscripts (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:8577:19)\n    at Object.parseExprSubscripts (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:8566:17)\n    at Object.parseMaybeUnary (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:8536:21)\n    at Object.parseExprOps (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:8402:23)\n    at Object.parseMaybeConditional (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:8375:23)\n    at Object.parseMaybeAssign (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:8325:21)\n    at Object.parseVar (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:10660:26)\n    at Object.parseVarStatement (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:10479:10)\n    at Object.parseStatementContent (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:10076:21)\n    at Object.parseStatement (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:10009:17)\n    at Object.parseBlockOrModuleBlockBody (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:10585:25)\n    at Object.parseBlockBody (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:10572:10)\n    at Object.parseTopLevel (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:9940:10)\n    at Object.parse (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:11447:17)\n    at parse (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\parser\\lib\\index.js:11483:38)\n    at parser (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\core\\lib\\transformation\\normalize-file.js:168:34)\n    at normalizeFile (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\core\\lib\\transformation\\normalize-file.js:102:11)\n    at runSync (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\core\\lib\\transformation\\index.js:44:43)\n    at runAsync (F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\core\\lib\\transformation\\index.js:35:14)\n    at F:\\WebAcademy_new\\static-builder-master\\node_modules\\@babel\\core\\lib\\transform.js:34:34\n    at processTicksAndRejections (internal/process/task_queues.js:75:11)");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _homework12_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./homework12.scss */ "./src/homework12/homework12.scss");
+/* harmony import */ var _homework12_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_homework12_scss__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _traffic_lighter_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./traffic-lighter.scss */ "./src/homework12/traffic-lighter.scss");
+/* harmony import */ var _traffic_lighter_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_traffic_lighter_scss__WEBPACK_IMPORTED_MODULE_1__);
+
+
+var trafficLighterRoot = document.querySelector(".traffic-lighter");
+var lights = trafficLighterRoot.querySelectorAll(".traffic-lighter__light");
+var togglerBtn = trafficLighterRoot.querySelector(".traffic-lighter__toggler");
+var isEnabled = false;
+var activeIndex = 0;
+var intervalId = 0;
+
+var toggleOffAll = function toggleOffAll() {
+  for (var i = 0; i < lights.length; i++) {
+    lights[i].classList.remove("active");
+  }
+};
+
+var activateLight = function activateLight() {
+  lights[activeIndex].classList.add("active");
+};
+
+var _loop = function _loop(i) {
+  lights[i].onclick = function () {
+    if (isEnabled) {
+      toggleOffAll();
+      activeIndex = i;
+      clearInterval(intervalId);
+      activateLight();
+      startAutoChange();
+    }
+  };
+};
+
+for (var i = 0; i < lights.length; i++) {
+  _loop(i);
+}
+
+var changeIndex = function changeIndex() {
+  if (activeIndex + 1 < lights.length) {
+    activeIndex += 1;
+  } else {
+    activeIndex = 0;
+  }
+};
+
+var toggleOffLighter = function toggleOffLighter() {
+  isEnabled = false;
+  toggleOffAll();
+  activeIndex = 0;
+  clearInterval(intervalId);
+};
+
+var startAutoChange = function startAutoChange() {
+  intervalId = setInterval(function () {
+    toggleOffAll();
+    changeIndex();
+    activateLight();
+  }, 1000);
+};
+
+var toggleOnLighter = function toggleOnLighter() {
+  isEnabled = true;
+  activateLight(0);
+  startAutoChange();
+};
+
+var togglerClickHandler = function togglerClickHandler() {
+  if (isEnabled) {
+    toggleOffLighter();
+  } else {
+    toggleOnLighter();
+  }
+};
+
+togglerBtn.onclick = togglerClickHandler;
+
+/***/ }),
+
+/***/ "./src/homework12/homework12.scss":
+/*!****************************************!*\
+  !*** ./src/homework12/homework12.scss ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./src/homework12/traffic-lighter.scss":
+/*!*********************************************!*\
+  !*** ./src/homework12/traffic-lighter.scss ***!
+  \*********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
 
 /***/ }),
 
